d60bf48b16685421e210c7375eb27a7de79e72d8
vidhanshu37 <kelawalavidhanshu@gmail.com>
2026-07-11 21:15:09 +0530

    Initial commit

---
+++ src/main/java/com/pr_reviewer/pr_reviewer/PrReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.pr_reviewer.pr_reviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrReviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrReviewerApplication.class, args);
+    }
+
+}
